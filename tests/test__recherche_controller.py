4b7f79fd1d3e5b4a4a7697ade1ffd64b25d0e4fa
Barlow <barlow.ma.1@ens.etsmtl.ca>
2025-06-09 06:42:09 +0000

    Add test

--- tests/test__recherche_controller.py
+++ tests/test__recherche_controller.py
@@ -1,7 +1,9 @@
 """"Test recherche produit"""
 from unittest.mock import patch
 from app.controllers.recherche_controller import (
-    menu_recherche_categorie
+    menu_recherche_categorie,
+    menu_recherche_id,
+    menu_recherche_nom
 )
 
 
@@ -15,3 +17,27 @@ def test_recherche_par_categorie():
 
     # Vérifie que la console a été appelé deux fois
     assert mock_afficher_produits.call_count == 2
+
+
+def test_recherche_par_id():
+    """"Test une recherche par id"""
+    id_ = "1"
+    with patch(
+            "views.recherche_view.demander_recherche_choix", side_effect=[id_, "back"]), \
+            patch("views.recherche_view.afficher_produits") as mock_afficher_produits:
+        menu_recherche_id("id")
+
+    # Vérifie que la console a été appelé une fois
+    assert mock_afficher_produits.call_count == 1
+
+
+def test_recherche_par_nom():
+    """"Test une recherche par id"""
+    nom = "Eau"
+    with patch(
+            "views.recherche_view.demander_recherche_choix", side_effect=[nom, "back"]), \
+            patch("views.recherche_view.afficher_produits") as mock_afficher_produits:
+        menu_recherche_id("nom")
+
+    # Vérifie que la console a été appelé une fois
+    assert mock_afficher_produits.call_count == 1
