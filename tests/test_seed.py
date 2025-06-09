bfd622aafe168330180a27185639d1c2c3d8615e
Barlow <barlow.ma.1@ens.etsmtl.ca>
2025-06-09 13:04:17 +0000

    Update test

--- tests/test_seed.py
+++ tests/test_seed.py
@@ -13,5 +13,5 @@ def test_seed_produit():
 def test_seed_inventaire():
     """Vérifie qu'un inventaire attendu existe"""
     run_all()
-    result = session.query(Inventaire).filter_by(id_produit="1").first()
+    result = session.query(Inventaire).filter_by(id_produit=1).first()
     assert result is not None
